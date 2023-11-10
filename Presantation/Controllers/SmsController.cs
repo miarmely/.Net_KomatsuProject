f8486d892622a8b40e0a5d7c511a543902a4985d
Mert Akdemir <comp.eng.m.akdemir@gmail.com>
2023-11-10 20:36:26 +0300

    (*) Sms Service Building.

---
+++ Presantation/Controllers/SmsController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace Presantation.Controllers
+{
+    [Route("api/services/[Controller]")]
+    [ApiController]
+    public class SmsController : ControllerBase
+    {
+        [HttpPost("send/1_1")]
+        public async Task<IActionResult> SendSms_1_1()
+        {
+            #region set http client
+            var httpClient = new HttpClient();
+            
+            httpClient.BaseAddress = new Uri("https://localhost:7091");
+            httpClient.DefaultRequestHeaders
+                .Accept
+                .Add(new MediaTypeWithQualityHeaderValue("application/soap+xml"));
+            #endregion
+
+            #region set string content
+            var contentInXml = @$"
+                <?xml version=""1.0"" encoding=""utf-8""?>
+                <soap12:Envelope xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" 
+                        xmlns:xsd=""http://www.w3.org/2001/XMLSchema"" 
+                        xmlns:soap12=""http://www.w3.org/2003/05/soap-envelope"">
+                  <soap12:Body>
+                    <SmsInsert_1_N xmlns=""http://83.66.137.24/PgApiWs"">
+                      <Username>Marubeni135</Username>
+                      <Password>Marubeni246!</Password>
+                      <SendDate>{DateTime.UtcNow}</SendDate>
+                      <ExpireDate>{DateTime.UtcNow.AddMinutes(5)}</ExpireDate>
+                      <Recepients>
+                        <string>5528093408</string>
+                      </Recepients>
+                      <Message>Deneme</Message>
+                    </SmsInsert_1_N>
+                  </soap12:Body>
+                </soap12:Envelope>";
+
+            var stringContent = new StringContent(
+                contentInXml,
+                Encoding.UTF8,
+                "application/soap+xml");
+            #endregion
+
+            #region send sms
+            var result = await httpClient.PostAsync(
+                "http://otpsms.postaguvercini.com/api_ws/smsservice.asmx",
+                stringContent);
+            #endregion
+
+            result.EnsureSuccessStatusCode();
+
+            return Ok();
+        }
+
+    }
+}
