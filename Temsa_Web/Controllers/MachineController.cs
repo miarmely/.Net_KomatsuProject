98765801956ab48fe4efbca856d29e386b339d85
Mert Akdemir <comp.eng.m.akdemir@gmail.com>
2024-04-05 12:03:54 +0300

    (*) Machine Category Page Is Designing.

--- Temsa_Web/Controllers/MachineController.cs
+++ Temsa_Web/Controllers/MachineController.cs
@@ -7,13 +7,8 @@ namespace Temsa_Web.Controllers
 	[Authorization("Editor,Admin,Editör,Yönetici")]
 	public class MachineController : Controller
 	{
-		public IActionResult Create()
-		{
-			return View("Create");
-		}
-		public IActionResult Display()
-		{
-			return View("Display", HttpContext.User.Claims);
-		}
+		public IActionResult Create() => View();
+		public IActionResult Display() => View("Display", HttpContext.User.Claims);
+		public IActionResult Category() => View();
 	}
 }
